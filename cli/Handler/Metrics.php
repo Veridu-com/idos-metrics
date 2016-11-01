90312186672c1aea612a93b4e20cb622004d58c3
Rafael Andreatta <eu@rafaa.me>
2016-11-01 20:11:01 -0200

    Fixes for metrics handler

--- cli/Handler/Metrics.php
+++ cli/Handler/Metrics.php
@@ -12,106 +12,26 @@ use Illuminate\Database\Connection as DbConnection;
 class Metrics
 {
     private $endpoints = [
-        'company' => [
-            'table' => 'company_metrics',
-            'actor' => 'identity',
-            'id'    => 'company'
-        ],
-        'company:credential' => [
-            'table' => 'credential_metrics',
-            'actor' => 'identity',
-            'id'    => 'credential'
-        ],
-        'company:hook' => [
-            'table' => 'hook_metrics',
-            'actor' => 'identity',
-            'id'    => 'hook'
-        ],
-        'company:invitation' => [
-            'table' => 'invitation_metrics',
-            'actor' => 'identity',
-            'id'    => 'invitation'
-        ],
-        'company:member' => [
-            'table' => 'member_metrics',
-            'actor' => 'identity',
-            'id'    => 'member'
-        ],
-        'company:permission' => [
-            'table' => 'permission_metrics',
-            'actor' => 'identity',
-            'id'    => 'permission'
-        ],
-        'company:setting' => [
-            'table' => 'setting_metrics',
-            'actor' => 'identity',
-            'id'    => 'setting'
-        ],
-        'profile:attribute' => [
-            'table' => 'attribute_metrics',
-            'actor' => 'credential',
-            'id'    => 'attribute'
-        ],
-        'profile:candidate' => [
-            'table' => 'candidate_metrics',
-            'actor' => 'credential',
-            'id'    => 'candidate'
-        ],
-        'profile:feature' => [
-            'table' => 'feature_metrics',
-            'actor' => 'credential',
-            'id'    => 'feature'
-        ],
-        'profile:flag' => [
-            'table' => 'flag_metrics',
-            'actor' => 'credential',
-            'id'    => 'flag'
-        ],
-        'profile:gate' => [
-            'table' => 'gate_metrics',
-            'actor' => 'credential',
-            'id'    => 'gate'
-        ],
-        'profile:process' => [
-            'table' => 'process_metrics',
-            'actor' => 'credential',
-            'id'    => 'process'
-        ],
-        'profile:raw' => [
-            'table' => 'raw_metrics',
-            'actor' => 'credential',
-            'id'    => 'source'
-        ],
-        'profile:reference' => [
-            'table' => 'reference_metrics',
-            'actor' => 'credential',
-            'id'    => 'reference'
-        ],
-        'profile:review' => [
-            'table' => 'review_metrics',
-            'actor' => 'identity',
-            'id'    => 'review'
-        ],
-        'profile:score' => [
-            'table' => 'score_metrics',
-            'actor' => 'credential',
-            'id'    => 'score'
-        ],
-        'profile:source' => [
-            'table' => 'source_metrics',
-            'actor' => 'credential',
-            'id'    => 'source'
-        ],
-        'profile:tag' => [
-            'table' => 'tag_metrics',
-            'actor' => 'identity',
-            'id'    => 'tag'
-        ],
-        'profile:task' => [
-            'table' => 'task_metrics',
-            'actor' => 'credential',
-            'id'    => 'task'
-        ]
+        'company'            => 'company_metrics',
+        'company:credential' => 'credential_metrics',
+        'company:hook'       => 'hook_metrics',
+        'company:invitation' => 'invitation_metrics',
+        'company:member'     => 'member_metrics',
+        'company:permission' => 'permission_metrics',
+        'company:setting'    => 'setting_metrics',
+        'profile:attribute'  => 'attribute_metrics',
+        'profile:candidate'  => 'candidate_metrics',
+        'profile:feature'    => 'feature_metrics',
+        'profile:flag'       => 'flag_metrics',
+        'profile:gate'       => 'gate_metrics',
+        'profile:process'    => 'process_metrics',
+        'profile:raw'        => 'raw_metrics',
+        'profile:reference'  => 'reference_metrics',
+        'profile:review'     => 'review_metrics',
+        'profile:score'      => 'score_metrics',
+        'profile:source'     => 'source_metrics',
+        'profile:tag'        => 'tag_metrics',
+        'profile:task'       => 'task_metrics'
     ];
 
     /**
@@ -139,11 +59,11 @@ class Metrics
             return false;
         }
 
-        $properties = $this->endpoints[$data['endpoint']];
+        $table = $this->endpoints[$data['endpoint']];
 
-        return $this->dbConnection->table($properties['table'])->insert([
-            $properties['actor'] . '_id' => $data['actor_id'],
-            $properties['id'] . '_id' => $data['id'],
+        return $this->dbConnection->table($table)->insert([
+            'actor_id' => $data['actor_id'],
+            'entity_id' => $data['entity_id'],
             'action' => $data['action'],
             'created_at' => date('Y-m-d H:i:s', $data['created_at'])
         ]);
@@ -153,43 +73,67 @@ class Metrics
      * Separate metrics by a given interval in seconds. Note that this method also
      * deletes metrics older than the specified seconds from the *_metrics table
      *
-     * @param int $seconds
+     * @param int    $seconds
      * @param string $toTablePostfix
      * @param string $fromTablePostfix
      */
     public function handleIntervalMetrics(int $seconds, string $toTablePostfix, string $fromTablePostfix = null) {
-        foreach ($this->endpoints as $endpoint => $properties) {
-            $query = '
-            WITH summary AS (
-                SELECT
-                    c.id,
-                    c.' . $properties['actor'] . '_id,
-                    c.action,
-                    c.created_at,
-                    ROW_NUMBER() OVER (
-                        PARTITION BY to_timestamp(floor((extract(\'epoch\' from c.created_at) / ' . $seconds . ' )) * ' . $seconds . ') AT TIME ZONE \'UTC\'
-                        ORDER BY c.id DESC) AS rowKey
-                    FROM ' . $properties['table'] . $fromTablePostfix . ' c
-                    WHERE c.created_at <= \'' . date('Y-m-d H:i:s', (time() - $seconds)) . '\'
-            )
-            SELECT s.* FROM summary s WHERE s.rowKey = 1';
-            $hourlyMetrics = $this->dbConnection->select($query);
+        foreach ($this->endpoints as $endpoint => $table) {
+            $endpoint = 'profile:source';
+            $table = 'source_metrics';
+            $metrics = $this
+                ->dbConnection
+                ->table($table . $fromTablePostfix)
+                ->where('created_at', '<=', date('Y-m-d H:i:s', (time() - $seconds)))
+                ->get(['*']);
 
-            if (empty($hourlyMetrics)) {
+            if (empty($metrics)) {
                 continue;
             }
 
-            foreach ($hourlyMetrics as $hourlyMetric) {
-                unset($hourlyMetric->id);
-                unset($hourlyMetric->rowkey);
+            $lastInsertedTimestamp = [];
+            $intervalMetrics = [];
+            foreach ($metrics as $metric) {
+                $group = $metric->actor_id . '-' . $metric->action;
+                $metric->created_at = ((int) (strtotime($metric->created_at) / $seconds)) * $seconds;
 
-                $this
-                    ->dbConnection
-                    ->table($properties['table'] . $toTablePostfix)
-                    ->insert((array) $hourlyMetric);
+                if (! isset($lastInsertedTimestamp[$group]) || $metric->created_at > ($lastInsertedTimestamp[$group] + $seconds - 1)) {
+                    if (! property_exists($metric, 'count')) {
+                        $metric->count = 1;
+                    }
+
+                    $intervalMetrics[$group][] = $metric;
+                    $lastInsertedTimestamp[$group] = $metric->created_at;
+
+                    continue;
+                }
+
+                $intervalMetrics[$group][count($intervalMetrics[$group]) - 1]->count += property_exists($metric, 'count') ? $metric->count : 1;
             }
 
-            $this->dbConnection->table($properties['table'] . $fromTablePostfix)->where('created_at', '<=', date('Y-m-d H:i:s', (time() - $seconds)))->delete();
+            $deleteIds = [];
+            foreach ($intervalMetrics as $group => $metrics) {
+                foreach ($metrics as $metric) {
+                    $deleteIds[] = $metric->id;
+
+                    unset($metric->id);
+                    unset($metric->entity_id);
+                    unset($metric->updated_at);
+
+                    $metric->created_at = date('Y-m-d H:i:s', $metric->created_at);
+
+                    $this
+                        ->dbConnection
+                        ->table($table . $toTablePostfix)
+                        ->insert((array) $metric);
+                }
+            }
+
+            $this
+                ->dbConnection
+                ->table($table . $fromTablePostfix)
+                ->whereIn('id', $deleteIds)
+                ->delete();
         }
     }
 
