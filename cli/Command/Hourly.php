a8eff1b8a9e7cd618c8e51f5848a7fa8b43edc9c
Flavio Heleno <flaviohbatista@gmail.com>
2017-04-11 16:43:18 -0300

    Changed flag shortcut. Test stuff man, test stuff..

--- cli/Command/Hourly.php
+++ cli/Command/Hourly.php
@@ -12,6 +12,7 @@ use Cli\Handler;
 use Cli\Utils\Logger;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
